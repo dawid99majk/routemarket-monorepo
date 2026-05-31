89a4f89c26eaabdcfd1def087eb4bd3060e6a007
dawid99majk <dawidmajka99@gmail.com>
2026-05-31 21:52:03 +0200

    fix(frontend): point CreatorAiStudio to new RouteBuilderV2

--- apps/frontend/src/pages/CreatorAiStudio.tsx
+++ apps/frontend/src/pages/CreatorAiStudio.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
-import { CreatorLayout } from '@/features/creator/components/CreatorLayout';
-import StudioMapEditor from '@/components/studio/StudioMapEditor';
+import React from 'react';
+import RouteBuilderV2 from './v2/RouteBuilderV2';
 
 export default function CreatorAiStudio() {
   return (
-    <CreatorLayout>
-      <div className="w-full h-[calc(100vh-80px)] flex flex-col mt-4">
-        <StudioMapEditor />
-      </div>
-    </CreatorLayout>
+    <div className="min-h-screen bg-black">
+      <RouteBuilderV2 />
+    </div>
   );
 }
