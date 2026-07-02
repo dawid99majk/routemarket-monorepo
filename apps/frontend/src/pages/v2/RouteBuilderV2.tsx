52077e8fe8d637ef7a652f5a59fa68943244033b
dawid99majk <dawidmajka99@gmail.com>
2026-07-02 08:58:23 +0200

    fix: Map TileLayer url using incorrect {r} template

--- apps/frontend/src/pages/v2/RouteBuilderV2.tsx
+++ apps/frontend/src/pages/v2/RouteBuilderV2.tsx
@@ -911,8 +911,8 @@ ${points}
           className="z-0"
         >
           <TileLayer
-            attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           
           <ClickableMap onMapClick={handleMapClick} />
